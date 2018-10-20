7399846aa0106760c5ae2236d1555e316ee34809
Jose Torres <jose.torres.tm@gmail.com>
2018-10-20 08:36:45 -0500

    wip

---
+++ handler/topic.go
@@ -0,0 +1,14 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/nu7hatch/gouuid"
+	"github.com/jltorresm/peerserver/types"
+)
+
+func init() {
+
+}
